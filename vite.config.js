64ef6c3451aa4e820a8f7e09704978ebed84f535
Kuki Amarilla <sc.amarilla@gmail.com>
2025-07-30 21:43:09 -0300

    fix: fix tailwind vite

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
     plugins: [
@@ -8,7 +8,7 @@ export default defineConfig({
             input: ['resources/css/app.css', 'resources/js/main.tsx'],
             refresh: true,
         }),
-        react(),
+        tailwindcss(),
     ],
     build: {
         rollupOptions: {
@@ -18,7 +18,6 @@ export default defineConfig({
                 },
             },
         },
-        chunkSizeWarningLimit: 1000,
     },
     optimizeDeps: {
         include: ['react', 'react-dom'],
