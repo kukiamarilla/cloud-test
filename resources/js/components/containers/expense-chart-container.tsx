4a7aa29c1d01fe66876f83fd7aaebd08b62d0154
Kuki Amarilla <sc.amarilla@gmail.com>
2025-07-30 23:07:02 -0300

    feat: add scaling for expenses chart

--- resources/js/components/containers/expense-chart-container.tsx
+++ resources/js/components/containers/expense-chart-container.tsx
@@ -10,35 +10,58 @@ interface DateExpense {
 }
 
 export const ExpenseChartContainer = () => {
+    const monthAgo = new Date();
+    monthAgo.setDate(monthAgo.getDate() - 30);
+
     const [expenses, setExpenses] = useState<DateExpense[]>([]);
-    const [loading, setLoading] = useState(false);
+    const [dateFrom, setDateFrom] = useState<Date>(monthAgo);
+
+    const handlePeriodChange = (period: string) => {
+        const daysLessMap = {
+            "1W": 7,
+            "1M": 30,
+            "3M": 90,
+            "6M": 180,
+            "1Y": 365,
+        }
+        const daysLess = daysLessMap[period as keyof typeof daysLessMap];
+        const dateFrom = new Date();
+        dateFrom.setDate(dateFrom.getDate() - daysLess);
+        setDateFrom(dateFrom);
+    }
 
     useEffect(() => {
-        setLoading(true);
         listMovements({
             page: 1,
             per_page: 1000,
+            date_from: dateFrom.toISOString(),
             type: 'expense',
         }).then((data) => {
-            const today = new Date();
-            const daysInMonth = new Date(today.getFullYear(), today.getMonth() + 1, 0).getDate();
             const expenses = data.data;
-            const dateExpenses: DateExpense[] = [];
-            for (let i = 1; i <= daysInMonth; i++) {
-                const date = new Date(today.getFullYear(), today.getMonth(), i);
-                const dateStr = date.toISOString().split('T')[0];
-                const amount = expenses.filter((movement: Movement) => movement.date.slice(0, 10) === dateStr).reduce((acc: number, movement: Movement) => acc + movement.amount, 0);
-                dateExpenses.push({ date: dateStr, amount });
+            const dateExpenses: Map<string, number> = new Map();
+            const today = new Date();
+            while (dateFrom.toISOString().split('T')[0] <= today.toISOString().split('T')[0]) {
+                const date = dateFrom.toISOString().split('T')[0];
+                dateExpenses.set(date, 0);
+                dateFrom.setDate(dateFrom.getDate() + 1);
             }
-            setExpenses(dateExpenses);
-        }).finally(() => {
-            setLoading(false);
+            expenses.forEach((expense: Movement) => {
+                const date = expense.date.slice(0, 10);
+                const dateAmount = dateExpenses.get(date) || 0;
+                dateExpenses.set(date, dateAmount + expense.amount);
+            });
+            const dateExpensesArray: DateExpense[] = [];
+            dateExpenses.forEach((amount, date) => {
+                dateExpensesArray.push({ date, amount });
+            });
+            dateExpensesArray.sort((a, b) => new Date(a.date).getTime() - new Date(b.date).getTime());
+            setExpenses(dateExpensesArray);
         });
-    }, []);
+    }, [dateFrom]);
 
     return (
         <div>
-            <ExpenseChart expenses={expenses} />
+            <ExpenseChart expenses={expenses} onPeriodChange={handlePeriodChange} />
         </div>
     )
 }
--- resources/js/components/custom/expense-chart.tsx
+++ resources/js/components/custom/expense-chart.tsx
@@ -5,6 +5,8 @@ import { useEffect, useState } from "react"
 import { Movement } from "@/model/movement";
 import { listMovements } from "@/service/movements";
 import { DatePicker } from "../ui/date-picker";
+import { ToggleGroup, ToggleGroupItem } from "../ui/toggle-group";
+import { Bold, Italic, Underline } from "lucide-react";
 
 interface DateExpense {
     date: string;
@@ -13,9 +15,15 @@ interface DateExpense {
 
 interface ExpenseChartProps {
     expenses: DateExpense[];
+    onPeriodChange: (period: string) => void;
 }
 
-export const ExpenseChart = ({ expenses}: ExpenseChartProps) => {
+export const ExpenseChart = ({ expenses, onPeriodChange }: ExpenseChartProps) => {
+    const [period, setPeriod] = useState("1M");
+    const handlePeriodChange = (value: string) => {
+        setPeriod(value);
+        onPeriodChange(value);
+    }
     const chartConfig = {
         amount: {
             label: "Monto",
@@ -29,6 +37,23 @@ export const ExpenseChart = ({ expenses}: ExpenseChartProps) => {
                     <CardTitle>Comportamiento de Gastos</CardTitle>
                 </CardHeader>
                 <CardContent>
+                    <ToggleGroup type="single" variant="outline" value={period} onValueChange={handlePeriodChange}>
+                        <ToggleGroupItem value="1W">
+                            1W
+                        </ToggleGroupItem>
+                        <ToggleGroupItem value="1M">
+                            1M
+                        </ToggleGroupItem>
+                        <ToggleGroupItem value="3M">
+                            3M
+                        </ToggleGroupItem>
+                        <ToggleGroupItem value="6M">
+                            6M
+                        </ToggleGroupItem>
+                        <ToggleGroupItem value="1Y">
+                            1Y
+                        </ToggleGroupItem>
+                    </ToggleGroup>
                     <ChartContainer config={chartConfig}>
                         <BarChart data={expenses}>
                             <ChartTooltip content={<ChartTooltipContent />} />
