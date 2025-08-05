a7198c44d6a485f93d4d5313b1b60ee097850612
Kuki Amarilla <sc.amarilla@gmail.com>
2025-08-04 23:52:30 -0300

    fix: correct date format in movements

--- resources/js/lib/utils.ts
+++ resources/js/lib/utils.ts
@@ -6,9 +6,22 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 export function formatDate(date: string) {
-    return new Date(date).toLocaleDateString('es-ES', {
-        day: '2-digit',
-        month: 'long',
-        year: 'numeric',
-    });
+  return new Date(setTimezone(date, -3)).toLocaleDateString('es-ES', {
+      day: '2-digit',
+      month: 'long',
+      year: 'numeric',
+  });
+}
+
+export function setTimezone(date: string, timezone: number){
+  let timezoneString: string;
+  if(timezone > 0){
+    timezoneString = `+${timezone.toString().padStart(2, '0')}:00`;
+  } else if(timezone < 0){
+    timezoneString = `-${Math.abs(timezone).toString().padStart(2, '0')}:00`;
+  }else{
+    timezoneString = `Z`;
+  }
+  const dateWithTimezone = `${date.substring(0,10)}T00:00:00${timezoneString}`;
+  return dateWithTimezone;
 }
