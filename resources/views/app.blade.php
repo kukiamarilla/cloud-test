6bdbf6fca64a6daabf62ad7b2c572a76b9f9a39f
Kuki Amarilla <sc.amarilla@gmail.com>
2025-08-09 12:47:31 -0300

    feat: add pwa manifest

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,6 +3,10 @@
     <head>
         <title>Escama - Gestión de finanzas personales</title>
         @vite(['resources/css/app.css', 'resources/js/main.tsx'])
+        <link rel="manifest" href="{{ asset('manifest.webmanifest') }}">
+        <meta name="theme-color" content="#ffffff">
+        <meta name="apple-mobile-web-app-title" content="Escama">
+        <link rel="apple-touch-icon" href="{{ asset('favicon.ico') }}">
         <link rel="icon" href="{{ asset('img/escama-logo.svg') }}" type="image/svg+xml">
     </head>
     <body>
