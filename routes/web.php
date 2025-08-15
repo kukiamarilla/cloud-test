d01929f893f9398a89eb9baec9ec0ce4b2019152
Kuki Amarilla <sc.amarilla@gmail.com>
2025-08-15 17:19:37 -0300

    feat: add redirection form / to /app

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', function () {
+    return redirect('/app');
+});
+
 Route::get('app/{any?}', function () {
     return view('app');
 })->where('any', '.*');
